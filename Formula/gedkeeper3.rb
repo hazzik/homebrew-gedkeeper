381906336fb44d6dd9567a7a17753a025e2a0d2d
Alex Zaytsev <hazzik@gmail.com>
2025-10-27 17:49:08 +1000

    fix: ln args

--- Formula/gedkeeper3.rb
+++ Formula/gedkeeper3.rb
@@ -9,21 +9,21 @@ class Gedkeeper3 < Formula
   depends_on "dotnet"
 
   def install
-    cd "projects/GKv3" do
-      system "dotnet", "build", "GEDKeeper3.sln", "-p:Configuration=Release", "-p:Platform=MacOS",
+    cd "projects/GKv3/GEDKeeper3" do
+      system "dotnet", "build", ".", "-p:Configuration=Release", "-p:Platform=MacOS",
 "-p:MacBuildBundle=true"
 
       # Debug: Show what was actually built in current directory
       system "find", ".", "-name", "*.app", "-type", "d"
 
-      prefix.install "GEDKeeper3/bin/MacOS/Release/GEDKeeper3.app"
+      prefix.install "bin/MacOS/Release/GEDKeeper3.app"
     end
 
     applications_dir = "/Applications"
-    ln "-s", "#{prefix}/GEDKeeper.app", "#{applications_dir}/GEDKeeper.app"
+    ln_s "#{prefix}/GEDKeeper3.app", "#{applications_dir}/GEDKeeper3.app"
   end
 
   test do
-    assert_path_exists prefix/"GEDKeeper.app", "GEDKeeper.app was not installed"
+    assert_path_exists prefix/"GEDKeeper3.app", "GEDKeeper3.app was not installed"
   end
 end
